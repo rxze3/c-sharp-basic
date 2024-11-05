9e51bfb267424db5f3cfd6a3235f645c826bd4bf
Marchenko Mykhailo <56231627+rxze3@users.noreply.github.com>
2024-11-05 17:05:48 +0100

    added project "phone2" which immitate mobile phone
    Added classes: Program, PhoneInfo, Contact and interface IContact. This program have 5 functions: adding new contacts with name and number, SMS to added contact, calling to added contact and look SMS/calls history.

---
+++ phone2/IContact.cs
@@ -0,0 +1,8 @@
+﻿namespace Phone
+{
+    public interface IContact
+    {
+        void SendSms(string message); 
+        void Calling();              
+    }
+}
---
+++ phone2/PhoneInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace Phone
+{
+    //information about phone
+    public class PhoneInfo
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SerialNumber { get; set; }
+
+        public PhoneInfo(string model, string color, string phoneNumber, string serialNumber)
+        {
+            Model = model;
+            Color = color;
+            PhoneNumber = phoneNumber;
+            SerialNumber = serialNumber;
+        }
+
+        //showing phone about
+        public void ShowPhoneInfo()
+        {
+            Console.WriteLine("\nPhone characteristic:");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Phone number: {PhoneNumber}");
+            Console.WriteLine($"Serial number: {SerialNumber}");
+        }
+    }
+}
